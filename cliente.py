61acf7d7a2571cf69165d21d83ebd45b2a9a43b7
PedroAugustoNascimento <150451782+PedroAugustoNascimento@users.noreply.github.com>
2024-09-02 16:36:00 -0300

    adicionando comentários

--- cliente.py
+++ cliente.py
@@ -1,18 +1,24 @@
 from socket import *
+#importando o JSON -> necessário para transformar um vetor em String
 import json
 
+#atribuindo o nome, a porta e o socket padrão para estabelecer conexão com o servidor
 serverName = 'localhost'
 serverPort = 12000
 cliente = socket(AF_INET, SOCK_STREAM)
 cliente.connect((serverName, serverPort))
 
+#recebendo a lista de produtos do servidor sem formatação
 produtos = json.loads(cliente.recv(1024).decode())
 
+#estabelecendo um limite de interações (máx 3 ofertas)
 contador = 0
 limite = 3
 
-def conexao():
+#função para receber a lista de produtos formatada e iniciar a negociação
+def negociacao():
     while True:
+        #formatação da lista de produtos
         print("Lista de produtos:")
         for produto in produtos:
             print("Código: {} | Nome: {} | Preço Inicial: {} | Estoque Disponível: {}".format(produto["codigo"], produto["nome"], produto["preco_inicial"], produto["estoque"]))
@@ -20,11 +26,13 @@ def conexao():
         codigo = int(input("Digite o código do produto que deseja comprar: "))
         preco = float(input("Digite o preço que deseja pagar: "))
 
+        #validando se o código escolhido pelo cliente está na lista de produtos
         produto_valido = next((p for p in produtos if p["codigo"] == codigo), None)
         if not produto_valido:
             print("Código de produto inválido. Tente novamente.")
             continue
-
+        
+        #oferta do cliente sendo enviada pelo JSON
         oferta = {"codigo": codigo, "preco": preco}
         cliente.send(json.dumps(oferta).encode())
 
@@ -40,4 +48,4 @@ def conexao():
             print("Você atingiu o limite de compras. Saindo...")
             break
 
-conexao()
+negociacao()
